425a4f8ad4e63c0e91214dd74eb8480554f68558
Marie Horsch <mhorsch2@ksu.edu>
2015-04-06 15:23:06 -0500

    deleted duplicate items

--- FitbitLogin/config/routes.rb
+++ FitbitLogin/config/routes.rb
@@ -18,8 +18,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  #root 'dashboard#index'
-  #root 'steps#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
