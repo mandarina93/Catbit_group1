a58457d29152b2b29001cdd34a2a2ba4d05bcef1
Marie Horsch <mhorsch2@ksu.edu>
2015-04-07 14:42:16 -0500

    Dates are fixded

--- FitbitLogin/db/schema.rb
+++ FitbitLogin/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312221250) do
+ActiveRecord::Schema.define(version: 20150407011557) do
+
+  create_table "goals", force: :cascade do |t|
+    t.integer  "goalName"
+    t.integer  "timeFrame"
+    t.integer  "goalAmount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "steps", force: :cascade do |t|
     t.date     "stepDate"
