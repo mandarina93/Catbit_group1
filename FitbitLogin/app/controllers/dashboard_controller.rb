200a95d3939f585412b00bf99aef24b4a53e0b5f
Marie Horsch <mhorsch2@ksu.edu>
2015-04-07 19:19:24 -0500

    Dashboard update

--- FitbitLogin/app/controllers/dashboard_controller.rb
+++ FitbitLogin/app/controllers/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class DashboardController < ApplicationController
   def index
+	client = user_client
+	#todays information
+	user_activities = client.activities_on_date 'today'
+	step = Step.step_data(user_activities["summary"], current_user.id)
+	distance = Distance.distance_data(user_activities["summary"], current_user.id)
+	calorie = Calorie.calorie_data(user_activities["summary"], current_user.id)
+	
 	@name = current_user.name
+	@stepToday = step.stepAmount
+	@distanceToday = distance.distanceAmount
+	@calorieToday = calorie.calorieOut
   end
 end
